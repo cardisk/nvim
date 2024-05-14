6125528020ec88d4c0db8e429d98560315ad2e41
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-05-14 19:02:34 +0200

    Adding keymaps for fzf.vim
    Refactoring the file to make it prettier.

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
+
+-- Generics -------------------------------------------------------------------
+
+-- Opening the NetRW
 vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
 
 -- Remapping $ and ^
@@ -18,29 +22,49 @@ vim.keymap.set("n", "V", "v")
 vim.keymap.set("n", "<C-a>", "ggVG")
 
 -- Save current buffer
-vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
+vim.keymap.set("n", "<C-s>", "<cmd> w <CR>")
 
 -- Save and quit
-vim.keymap.set("n", "<C-q>", "<cmd>wq<CR>")
+vim.keymap.set("n", "<C-q>", "<cmd> wq <CR>")
 
--- Splitting panes
+-------------------------------------------------------------------------------
+
+-- Splitting panes ------------------------------------------------------------
+
 -- Split horizontally the same window
-vim.keymap.set("n", "|", "<cmd>vsplit<CR><C-W><C-W>")
--- Split vertically the same window
-vim.keymap.set("n", "_", "<cmd>split<CR>")
--- Split horizontally a new empty window
-vim.keymap.set("n", "<leader>n", "<cmd>new<CR>")
+vim.keymap.set("n", "|", "<cmd> vsplit <CR><C-W><C-W>")
 
--- Moving cursor through windows
+-- Split vertically the same window
+vim.keymap.set("n", "_", "<cmd> split <CR>")
+
+-- Split horizontally a new empty window
+vim.keymap.set("n", "<leader>n", "<cmd> new <CR>")
+
+-------------------------------------------------------------------------------
+
+-- Moving cursor through windows ----------------------------------------------
+
 -- Going to the upper window
 vim.keymap.set("n", "<leader><Up>", "<C-W><Up>")
--- vim.keymap.set("n", "<leader>k", "<C-W><Up>")
+
 -- Going to the bottom window
 vim.keymap.set("n", "<leader><Down>", "<C-W><Down>")
--- vim.keymap.set("n", "<leader>j", "<C-W><Down>")
+
 -- Going to the left window
 vim.keymap.set("n", "<leader><Left>", "<C-W><Left>")
--- vim.keymap.set("n", "<leader>l", "<C-W><Left>")
+
 -- Going to the right window
 vim.keymap.set("n", "<leader><Right>", "<C-W><Right>")
--- vim.keymap.set("n", "<leader>h", "<C-W><Right>")
+
+-------------------------------------------------------------------------------
+
+-- Plugins remaps -------------------------------------------------------------
+
+-- fzf.vim
+vim.keymap.set("n", "<C-f>", "<cmd> Files <CR>")
+vim.keymap.set("n", "<C-b>", "<cmd> Buffers <CR>")
+vim.keymap.set("n", "<C-g>", "<cmd> GFiles <CR>")
+vim.keymap.set("n", "<C-j>", "<cmd> Jumps <CR>")
+vim.keymap.set("n", "<C-l>", "<cmd> Lines <CR>")
+
+-------------------------------------------------------------------------------
