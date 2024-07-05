faa85a8d7d0a87ac7b31a1e2401229617e4eb43d
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-07-05 23:51:19 +0200

    [lua/lspzero.lua] adding support for swift lsp

--- lua/lspzero.lua
+++ lua/lspzero.lua
@@ -1,5 +1,23 @@
 local lsp_zero = require('lsp-zero')
 
+local swift_lsp = vim.api.nvim_create_augroup("swift_lsp", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "swift" },
+	callback = function()
+		local root_dir = vim.fs.dirname(vim.fs.find({
+			"Package.swift",
+			".git",
+		}, { upward = true })[1])
+		local client = vim.lsp.start({
+			name = "sourcekit-lsp",
+			cmd = { "sourcekit-lsp" },
+			root_dir = root_dir,
+		})
+		vim.lsp.buf_attach_client(0, client)
+	end,
+	group = swift_lsp,
+})
+
 lsp_zero.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
