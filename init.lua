06016d12ca2cd7d94e940834d32ec758eb52965d
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-11-11 21:41:21 +0100

    Adding other C3 extensions and adding auto comments inside them

--- init.lua
+++ init.lua
@@ -19,4 +19,8 @@ vim.cmd.colorscheme('ayu-dark')
 
 -- XTASM syntax highlighting
 vim.cmd([[ autocmd BufRead,BufNewFile *.xt set filetype=xtasm ]])
-vim.cmd([[ autocmd BufNewFile,BufRead *.c3 setfiletype c3 ]])
+vim.cmd([[ autocmd BufNewFile,BufRead *.c3,*.c3t,*.c3i set filetype=c3 ]])
+
+-- Vim-comentary auto comments for C3
+vim.cmd([[ autocmd FileType c3 setlocal commentstring=//\ %s ]])
+
