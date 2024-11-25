055bbee21df42d0db3da153997f289b47fb5866c
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-11-25 23:19:09 +0100

    C3 treesitter

--- init.lua
+++ init.lua
@@ -19,8 +19,27 @@ vim.cmd.colorscheme('ayu-dark')
 
 -- XTASM syntax highlighting
 vim.cmd([[ autocmd BufRead,BufNewFile *.xt set filetype=xtasm ]])
-vim.cmd([[ autocmd BufNewFile,BufRead *.c3,*.c3t,*.c3i set filetype=c3 ]])
+
+-- C3 syntax highlighting
+-- vim.cmd([[ autocmd BufNewFile,BufRead *.c3,*.c3t,*.c3i set filetype=c3 ]])
 
 -- Vim-comentary auto comments for C3
 vim.cmd([[ autocmd FileType c3 setlocal commentstring=//\ %s ]])
 
+-- C3 treesitter
+vim.filetype.add({
+  extension = {
+    c3 = "c3",
+    c3i = "c3",
+    c3t = "c3",
+  },
+})
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.c3 = {
+  install_info = {
+    url = "https://github.com/c3lang/tree-sitter-c3",
+    files = {"src/parser.c", "src/scanner.c"},
+    branch = "main",
+  },
+}
