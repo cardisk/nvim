9999565feaf60570ba2178241322a4eba8718349
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-11-25 23:19:59 +0100

    Fix indentation

--- init.lua
+++ init.lua
@@ -28,18 +28,18 @@ vim.cmd([[ autocmd FileType c3 setlocal commentstring=//\ %s ]])
 
 -- C3 treesitter
 vim.filetype.add({
-  extension = {
-    c3 = "c3",
-    c3i = "c3",
-    c3t = "c3",
-  },
+    extension = {
+        c3 = "c3",
+        c3i = "c3",
+        c3t = "c3",
+    },
 })
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.c3 = {
-  install_info = {
-    url = "https://github.com/c3lang/tree-sitter-c3",
-    files = {"src/parser.c", "src/scanner.c"},
-    branch = "main",
-  },
+    install_info = {
+        url = "https://github.com/c3lang/tree-sitter-c3",
+        files = {"src/parser.c", "src/scanner.c"},
+        branch = "main",
+    },
 }
